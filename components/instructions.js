93f2b83345c9e8b7ee33da1d16f5a792217b5ccd
Rob <106797059+rpribau@users.noreply.github.com>
2024-03-11 23:36:13 -0600

    Instructions edited

--- components/instructions.js
+++ components/instructions.js
@@ -1,39 +1,65 @@
-import {
-    List,
-    ListItem,
-    ListIcon,
-    OrderedList,
-    UnorderedList,
-  } from '@chakra-ui/react'
+import React from 'react';
+import { List, ListItem, ListIcon, Box } from '@chakra-ui/react';
+import { MdCheckCircle } from 'react-icons/md';
+import { PiWarningCircleFill } from "react-icons/pi";
+import { Text } from '@chakra-ui/react';
 
-import { MdCheckCircle, MdSettings } from 'react-icons/md'
-
-  const Instructions = () => { 
-    return (
+const Instructions = () => {
+  return (
+    <Box display="flex">
+      {/* First List */}
+      <Box flex="1" marginRight="4">
         <List spacing={3} mt={4}>
-                        <ListItem>
-                            <ListIcon as={MdCheckCircle} color='green.500' />
-                            Date (object)
-                        </ListItem>
-                        <ListItem>
-                            <ListIcon as={MdCheckCircle} color='green.500' />
-                            Amount (float64)
-                        </ListItem>
-                        <ListItem>
-                            <ListIcon as={MdCheckCircle} color='green.500' />
-                            Is_holiday (int64)
-                        </ListItem>
-                        {/* You can also use custom icons from react-icons */}
-                        <ListItem>
-                            <ListIcon as={MdCheckCircle} color='green.500' />
-                            Sales (int64)
-                        </ListItem>
-                        <ListItem>
-                            <ListIcon as={MdCheckCircle} color='green.500' />
-                            Weekday (int64)
-                        </ListItem>
-                        </List>
-    );
+            <Text mt={4} as='b'>RandomForest v.1: </Text>
+          <ListItem>
+            <ListIcon as={MdCheckCircle} color='green.500' />
+            Date (object)
+          </ListItem>
+          <ListItem>
+            <ListIcon as={MdCheckCircle} color='green.500' />
+            Location (object)
+          </ListItem>
+          <ListItem>
+            <ListIcon as={MdCheckCircle} color='green.500' />
+            Product (int64)
+          </ListItem>
+            <ListItem>
+                <ListIcon as={MdCheckCircle} color='green.500' />
+            Amount (int64)
+            </ListItem>
+            <ListItem>
+                <ListIcon as={MdCheckCircle} color='green.500' />
+            Unit_price (int64)
+            </ListItem>
+            <ListItem>
+                <ListIcon as={MdCheckCircle} color='green.500' />
+            Weekday (object)
+            </ListItem>
+        </List>
+      </Box>
+      
+      {/* Second List */}
+      <Box flex="1">
+        <List spacing={3} mt={4}>
+        <Text mt={4} as='b'>RandomForest v.2, SARIMAX, XGBoost: </Text>
+          <ListItem>
+            <ListIcon as={PiWarningCircleFill} color='yellow.500' />
+            Los dias de la semana tienen que ser su propia columna marcando 1 como dia, en caso de que no lo sea se marca 0.
+          </ListItem>
+          <ListItem>
+            <ListIcon as={PiWarningCircleFill} color='yellow.500' />
+            Las fechas se tienen que separar en año, mes y dia con su propia columna.
+          </ListItem>
+            <ListItem>
+                <ListIcon as={PiWarningCircleFill} color='red.500' />
+                Las ubicaciones no se encuentran en este modelo, los datos son generalizados por cada dia.
+            </ListItem>
+
+          {/* Add more items here if needed */}
+        </List>
+      </Box>
+    </Box>
+  );
 }
 
 export default Instructions;
